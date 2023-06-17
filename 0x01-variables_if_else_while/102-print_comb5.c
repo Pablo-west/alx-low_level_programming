f631ecd9197a1dcfdfb9e13184ae8553aa56ce3f
Pablo-west <edemkng4@gmail.com>
2023-06-17 00:08:07 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int number1, number2;
+
+for (number1 = 0; number1 <= 99; number1++)
+{
+for (number2 = 0; number2 <= 99; number2++)
+{
+if (number1 <= number2)
+{
+putchar('0' + (number1 / 10));
+putchar('0' + (number1 % 10));
+putchar(' ');
+putchar('0' + (number2 / 10));
+putchar('0' + (number2 % 10));
+if (!(number1 == 99 && number2 == 99))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
