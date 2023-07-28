9a22335e69e3f8f8290e3a4e119dc86f9e5749fa
Pablo-west <edemkng4@gmail.com>
2023-07-27 17:10:41 -0700

    :Task2

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,13 +9,12 @@
  */
 size_t list_len(const list_t *head)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (head)
-    {
-        count++;
-        head = head->next;
-    }
-
-    return count;
+while (head)
+{
+count++;
+head = head->next;
+}
+return (count);
 }
