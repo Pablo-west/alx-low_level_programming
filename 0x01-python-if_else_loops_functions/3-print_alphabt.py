0fff1d97e719aa4377bc612e42b3756e8a1ca606
Pablo-west <edemkng4@gmail.com>
2023-09-19 23:46:42 -0700

    done

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    if chr(char) not in 'qe':
-        print(f"{chr(char)}", end='')
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-# 4-print_hexa.py
-
-"""Print numbers 0 to 98 in decimal and hexadecimal."""
-for num in range(99):
-    print("{:d} = 0x{:02x}".format(num, num))
